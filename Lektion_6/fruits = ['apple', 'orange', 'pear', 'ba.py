5974c02949c210ef1d06c2104faf5ff08a170f97
Michael Levin <mickelew@gmail.com>
2020-03-27 08:44:22 +0100

    En fil borttagen

--- Lektion_6/fruits = ['apple', 'orange', 'pear', 'ba.py
+++
@@ -1,9 +0,0 @@
-fruits = ['apple', 'orange', 'pear', 'banana', 'grapes']
-basket_space = int(input("Hur många frukter får plats i korgen?: "))
-my_basket = []
-
-while len(my_basket) <= basket_space-1:
-    for x in fruits:
-        if len(my_basket) < basket_space:
-            my_basket.append(x)
-print(my_basket)
